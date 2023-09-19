4d998493133844eba1dd8edab570df344dcef0c6
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-09-19 12:06:49 +0200

    Setup project (spring initializr, web, lombok, web reactive)

---
+++ src/main/java/com/example/foodmenuapp/FoodMenuAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodmenuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMenuAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodMenuAppApplication.class, args);
+    }
+
+}
